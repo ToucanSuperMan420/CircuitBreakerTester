fa5306b953a51438df32f21c29d5918742ef4686
ToucanSuperMan420 <64911008+ToucanSuperMan420@users.noreply.github.com>
2020-05-06 16:42:26 +0200

    Added misssing files

---
+++ WcfService/Service1.svc.cs
@@ -0,0 +1,21 @@
+﻿using System.ServiceModel;
+
+namespace WcfService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
+    public class Service1 : IService1
+    {
+        private static bool _isAlive = true;
+
+        public string DoWork()
+        {
+            return _isAlive ? "Connexion Successfull" : throw new EndpointNotFoundException();
+        }
+
+        public void SetIsAlive(bool IsAlive)
+        {
+            _isAlive = IsAlive;
+        }
+    }
+}
